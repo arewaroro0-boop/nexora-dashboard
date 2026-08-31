b4d99a02a341e315d5cecf9242bcfe2228e01fa1
arewaroro0-boop <arewaroro0@gmail.com>
2026-08-31 21:50:05 +0100

    script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
+const menuButton = document.getElementById("menuButton");
+const sidebar = document.querySelector(".sidebar");
 
+if (menuButton && sidebar) {
+    menuButton.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+    });
+}
+
+
+const notificationButton =
+    document.getElementById("notificationButton");
+
+const notificationPanel =
+    document.getElementById("notificationPanel");
+
+const closeNotifications =
+    document.getElementById("closeNotifications");
+
+
+if (notificationButton && notificationPanel) {
+    notificationButton.addEventListener("click", () => {
+        notificationPanel.classList.toggle("show");
+    });
+}
+
+
+if (closeNotifications && notificationPanel) {
+    closeNotifications.addEventListener("click", () => {
+        notificationPanel.classList.remove("show");
+    });
+}
+
+
+document.addEventListener("click", (event) => {
+
+    if (
+        notificationPanel &&
+        notificationButton &&
+        notificationPanel.classList.contains("show") &&
+        !notificationPanel.contains(event.target) &&
+        !notificationButton.contains(event.target)
+    ) {
+        notificationPanel.classList.remove("show");
+    }
+
+});
